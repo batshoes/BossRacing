e21f6f442237540862a1dbb11e71cdf4a19c6db1
James Middlemiss <james.o.middlemiss@gmail.com>
2018-12-12 11:54:34 +1100

    add pretty links, css for nav, Race schema and Schedule

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  # Users
+  devise_scope :user do
+    get "/log-in", to: "devise/sessions#new", as: 'log_in' # custom path to login/sign_in
+    get "/sign-up", to: "devise/registrations#new", as: "sign_up" # custom path to sign_up/registration
+  end
+  devise_for :users
+
+  resources :races
+
+  get '/schedule', to: 'schedule#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_042200) do
+ActiveRecord::Schema.define(version: 2018_12_11_120811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "races", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
