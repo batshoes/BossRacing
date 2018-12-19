b379bc8501c66ba1ad464d7703239a4597081c7f
James Middlemiss <james.o.middlemiss@gmail.com>
2018-12-19 18:14:23 +1100

    create event

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_120811) do
+ActiveRecord::Schema.define(version: 2018_12_19_070750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.bigint "race_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_events_on_race_id"
+  end
 
   create_table "races", force: :cascade do |t|
     t.string "name"
