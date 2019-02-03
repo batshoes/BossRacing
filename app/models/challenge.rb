597aae0d39223dbf9ade7d18ae617bf53d234ead
James Middlemiss <james.middlemiss@valiant.finance>
2019-02-03 15:21:58 +1100

    not even sure anymore

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -6,13 +6,6 @@ class Challenge < ApplicationRecord
     completed: 'completed'
   }
 
-<<<<<<< HEAD
-=======
-  belongs_to :challenger, class_name: 'User'
-  belongs_to :challengee, class_name: 'User'
-
-  has_many :users
->>>>>>> 9681cf1104b94a532220c8953cf9bb85e9a600b0
   belongs_to :event
 
   belongs_to :challenger, class_name: 'User'
@@ -22,18 +15,12 @@ class Challenge < ApplicationRecord
 
   validates_presence_of :event_id, :start_time, :challenger_id, :challengee_id
 
-  scope :upcoming, ->(limit, status){ where(status: status).limit(limit) }
+  scope :upcoming, ->(limit, status) { where(status: status).limit(limit) }
 
   def event_name
     event.name
   end
 
-<<<<<<< HEAD
-  def users
-    return [challenger, challengee]
-  end
-
-=======
   def challenger_name
     challenger.username
   end
@@ -41,5 +28,4 @@ class Challenge < ApplicationRecord
   def challengee_name
     challengee.username
   end
->>>>>>> 9681cf1104b94a532220c8953cf9bb85e9a600b0
 end
